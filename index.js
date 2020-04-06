e624a3b86cd95793f31dc92571c89d8779ea7baf
Kumu <kumusadia02@gmail.com>
2020-04-07 01:03:21 +0600

    node mongo

--- index.js
+++ index.js
@@ -1,63 +1,77 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
 
 const app = express();
+
+const uri = process.env.DB_PATH;
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
 app.use(cors());
 app.use(bodyParser.json());
 
-
-//database connection
-const uri = process.env.DB_PATH;
-
-let client = new MongoClient(uri, { useNewUrlParser: true });
-const users = ["Asad", 'Moin', "Sabed" , "Susmita" , "Sohana", "Sabana"];
-
-
-app.get('/menu',(req, res) => {
-    client = new MongoClient(uri, { useNewUrlParser: true });
-    client.connect(err => {
-        const collection = client.db("redOnion").collection("menu");
-        collection.find().toArray((err, documents) => {
-            if(err){
-                console.log(err)
-                res.status(500).send({message:err});
-            }
-            else{
-                res.send(documents);
-            }
-        });
-        client.close();
-      });
+app.post("/menu", (req, res) => {
+  const menu = req.body;
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((error) => {
+    const collection = client.db("redOnion").collection("menu");
+    collection.insert(menu, (err, result) => {
+      if (err) {
+        console.log(err);
+        console.log(error)
+        res.status(500).send({ message: err });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+    client.close();
+  });
 });
 
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id ;
-    const name = users[id];
-    res.send({id, name});
+app.post("/placeOrder", (req, res) => {
+  const orderDetails = req.body;
+  orderDetails.orderTime = new Date();
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((error) => {
+    const collection = client.db("redOnion").collection("orders");
+    collection.insertOne(orderDetails, (err, result) => {
+      if (err) {
+        console.log(err);
+        console.log(error)
+        res.status(500).send({ message: err });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+    client.close();
+  });
 });
 
-app.post('/addMenu', (req, res) => {
-   
-    const menu = req.body;
-    client = new MongoClient(uri, { useNewUrlParser: true });
-    client.connect(err => {
-        const collection = client.db("redOnion").collection("menu");
-        collection.insertOne(menu, (err, result) => {
-            if(err){
-                console.log(err)
-                res.status(500).send({message:err});
-            }
-            else{
-                res.send(result.ops[0]);
-            }
-        });
-        client.close();
-      });
+app.get("/addMenu", (req, res) => {
+  //const category = req.params.category;
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((error) => {
+    const collection = client.db("redOnion").collection("menu");
+    collection.find().toArray((err, documents) => {
+      if (err) {
+        console.log(err);
+        console.log(error)
+        res.status(500).send({ message: err });
+      } else {
+        res.send(documents);
+      }
+    });
+    client.close();
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Red Onion Server</h1>");
 });
 
 const port = process.env.PORT || 4200;
-app.listen(4200, () => console.log('Listening to port 4200'));
+
+app.listen(port, () => { console.log("Listening at port 4200");
+});
